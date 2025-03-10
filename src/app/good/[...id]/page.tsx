0e4337ff0e4f4c3f5a4e9fe205c6649e89218028
suhyun5252 <o52o.suhyun@gmail.com>
2025-03-10 11:32:59 +0900

    환경설정 및 라우터, console.log

---
+++ src/app/good/[...id]/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  console.log(id);
+  return <div> {id}번 : 제품 상세페이지</div>;
+}
---
+++ src/app/search/not-found.tsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <div>쿼리가 잘못된 경로 입니다. 404 페이지</div>;
+}
